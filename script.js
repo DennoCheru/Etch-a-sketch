d70b25369201c73e49e52497d797ae6d4b841f50
Cheru <dennocheruh@gmail.com>
2024-11-06 15:01:47 +0300

    Create Etch-a-sketch
    Created Etch-a-sketch game, with multiple color changes
    on hover.

---
+++ script.js
@@ -0,0 +1,49 @@
+const container = document.querySelector(".container")
+const newGridButton = document.querySelector("#newGridButton")
+
+newGridButton.addEventListener('click', newGrid)
+
+function createGrid(squaresPerSide) {
+    const squareSize = 500 / squaresPerSide;
+
+    container.textContent = '';
+
+    for (let i =0; i < squaresPerSide * squaresPerSide; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.style.opacity = 0.1;
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = getRandomColor();
+            let currentOpacity = parseFloat(square.style.opacity);
+            if (currentOpacity < 1) {
+                square.style.opacity = currentOpacity + 0.1;
+            }
+        });
+
+        container.appendChild(square);
+    }
+}
+
+function newGrid() {
+    let squaresPerSide = parseInt(prompt("Enter the number of squares per side. (Maximum 100): "));
+
+    if(isNaN(squaresPerSide) || squaresPerSide < 1 || squaresPerSide> 100) {
+        alert("Please enter a number between 1 and 100");
+    } else {
+        createGrid(squaresPerSide);
+    }
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+createGrid(16);
